4eb7e981d300e1be4cc805c70fd59068aa0520cc
Hannah Carnes <hecarnes@gmail.com>
2018-04-19 15:50:16 -0400

    wip

--- top_ten/lib/top_ten.rb
+++ top_ten/lib/top_ten.rb
@@ -5,4 +5,5 @@ require "selenium-webdriver"
 
 require_relative "./top_ten/version"
 require_relative "./top_ten/cli"
-require_relative "./top_ten/products"
+require_relative "./top_ten/product"
+require_relative "./top_ten/scraper"
--- top_ten/lib/top_ten/cli.rb
+++ top_ten/lib/top_ten/cli.rb
@@ -1,38 +1,18 @@
-class TopTen::CLI
+class CLI
+
+  def initialize
+    @scraper = Scraper.new
+  end
 
   def call
     list_products
-    menu
-    goodbye
   end
 
   def list_products
     puts "Everlane Top 10 Best Sellers:"
-    @products = TopTen::Products.today
-    @products.each.with_index(1) do |products, i|
-      puts "#{i}. #{products.name} - #{products.price}"
+    @products = @scraper.scrape_products
+    @products.each.with_index(1) do |product, i|
+      puts "#{i}. #{product.name}"
     end
   end
-
-  def menu
-    input = nil
-    while input != "exit"
-      puts "Which item # would you like to know more about? Enter 'list' to review the top-sellers or 'exit' to leave."
-      input = gets.strip.downcase
-
-      if input.to_i > 0
-        the_product = @products[input.to_i - 1]
-        puts "#{the_product.name} - #{the_product.price}"
-      elsif input == "list"
-        list_products
-      else
-        puts "Not sure what you mean, type 'exit' to leave, 'list' to view the top-sellers, or 1-10 to view a particular item."
-      end
-    end
-  end
-
-  def goodbye
-    puts "Thanks for visiting! Swing by anytime for more information on Everlane's most popular items."
-  end
-
 end
