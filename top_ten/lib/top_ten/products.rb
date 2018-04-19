032d60e7976956496524d558d14412b045703cf0
Annie Bourke <aabourke@gmail.com>
2018-04-19 18:38:41 +0000

    moves scraping methods from Products class to new Scraper class

--- top_ten/lib/top_ten/products.rb
+++ top_ten/lib/top_ten/products.rb
@@ -6,32 +6,7 @@ class TopTen::Products
     @url = url
     @price = price
     @colors = colors
-    @description = description 
-  end
-
-  def self.today
-    #scrape Everlane product pages and return product details based on that data
-    self.scrape_products
-  end
-
-  def self.scrape_products
-    products = []
-
-    products << self.scrape_first
-    #go to Everlane, find the product
-    #extract the properties of the product
-    #instantiate a product
-    #repeat for entire top10 list
-    products
-  end
-
-  def self.scrape_first
-#    options = Selenium::WebDriver::Chrome::Options.new(args: ['headless'])
-#    @driver = Selenium::WebDriver.for(:chrome, options: options)
-    @driver.get('https://www.everlane.com/collections/womens-bestsellersv2')
-
-#    doc = Nokogiri::HTML(open("https://www.everlane.com/collections/womens-bestsellersv2"))
-    binding.pry
+    @description = description
   end
 
 end
